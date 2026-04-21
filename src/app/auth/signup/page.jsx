3e89909191182ff755a3696644559377962d2a86
JowelislamHabib <jowel@bintofajjal.com>
2026-04-21 22:35:30 +0600

    45-4 Setup auth route, HeroUI and Gravity UI icons
    feat: add authentication and signup functionality
    
    - Updated package.json to include new dependencies for UI components and authentication.
    - Created API route for authentication using better-auth.
    - Implemented a signup page with UI components from @heroui/react.
    - Added global styles for @heroui/styles.
    - Created an auth client for better-auth integration.
    - Enhanced auth configuration to enable email and password authentication.

---
+++ src/app/auth/signup/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  Button,
+  Description,
+  FieldError,
+  Form,
+  Input,
+  Label,
+  TextField,
+} from "@heroui/react";
+import { Check } from "@gravity-ui/icons";
+
+const SignUpPage = () => {
+  return <div>Please Sign Up</div>;
+};
+
+export default SignUpPage;
