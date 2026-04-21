d389d21cb1c8d5a0de144676aa8802fec113cf6c
JowelislamHabib <jowel@bintofajjal.com>
2026-04-22 01:52:08 +0600

    [BUG] 45-5 Implement sign up form and sign up user

--- src/app/auth/signup/page.jsx
+++ src/app/auth/signup/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import {
   Button,
@@ -9,9 +10,103 @@ import {
   TextField,
 } from "@heroui/react";
 import { Check } from "@gravity-ui/icons";
+import { authClient } from "@/lib/auth-client";
 
 const SignUpPage = () => {
-  return <div>Please Sign Up</div>;
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const userData = Object.fromEntries(formData.entries());
+    console.log("Form submitted with:", userData);
+
+    const { data, error } = await authClient.signUp.email({
+      name: userData.name,
+      email: userData.email,
+      password: userData.password,
+      callbackURL: "/",
+    });
+
+    console.log("Sign up response:", { data, error });
+    if (error) {
+      alert("Error signing up: " + error.message);
+    }
+    if (data) {
+      alert(
+        "Sign up successful! Please check your email to verify your account.",
+      );
+    }
+  };
+
+  return (
+    <>
+      <h1 className="Card.Title"> Please Sign up</h1>
+      <Form className="flex w-96 flex-col gap-4" onSubmit={onSubmit}>
+        <TextField
+          isRequired
+          name="name"
+          type="text"
+          validate={(value) => {
+            if (value.length < 3) {
+              return "Username must be at least 3 characters";
+            }
+          }}
+        >
+          <Label>Username</Label>
+          <Input placeholder="Username" />
+          <FieldError />
+        </TextField>
+        <TextField
+          isRequired
+          name="email"
+          type="email"
+          validate={(value) => {
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+              return "Please enter a valid email address";
+            }
+            return null;
+          }}
+        >
+          <Label>Email</Label>
+          <Input placeholder="john@example.com" />
+          <FieldError />
+        </TextField>
+        <TextField
+          isRequired
+          minLength={8}
+          name="password"
+          type="password"
+          validate={(value) => {
+            if (value.length < 8) {
+              return "Password must be at least 8 characters";
+            }
+            if (!/[A-Z]/.test(value)) {
+              return "Password must contain at least one uppercase letter";
+            }
+            if (!/[0-9]/.test(value)) {
+              return "Password must contain at least one number";
+            }
+            return null;
+          }}
+        >
+          <Label>Password</Label>
+          <Input placeholder="Enter your password" />
+          <Description>
+            Must be at least 8 characters with 1 uppercase and 1 number
+          </Description>
+          <FieldError />
+        </TextField>
+        <div className="flex gap-2">
+          <Button type="submit">
+            <Check />
+            Submit
+          </Button>
+          <Button type="reset" variant="secondary">
+            Reset
+          </Button>
+        </div>
+      </Form>
+    </>
+  );
 };
 
 export default SignUpPage;
